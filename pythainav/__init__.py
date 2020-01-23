e09cb6b61ef5dfb78f8f322f9cd1a51ac9157b72
Nutchanon Ninyawee <me@nutchanon.org>
2020-01-23 16:34:04 +0700

    👍add todo

--- pythainav/__init__.py
+++ pythainav/__init__.py
@@ -3,6 +3,7 @@ from requests_html import HTMLSession
 def get_nav(fund: str):
 
     url = "https://www.finnomena.com/fund/"
+    # TODO: parse ilegal fund name "BP33/19" -> "BP33%2F19"
     url = url + fund
     session = HTMLSession()
     r = session.get(url)
