95d32e19ab4c0700e49fbfc42f5916198a4716ac
Nutchanon Ninyawee <me@nutchanon.org>
2020-01-25 02:07:29 +0700

    reformat code

--- pythainav/sources.py
+++ pythainav/sources.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
+
 class Source(ABC):
-    
     @abstractmethod
     def get(self, fund: str):
         pass
@@ -10,11 +10,11 @@ class Source(ABC):
     def list(self):
         pass
 
-class Finnomena(Source):
 
+class Finnomena(Source):
     def __init__(self):
         super().__init__()
-    
+
     def get(self, fund: str):
         from requests_html import HTMLSession
 
@@ -24,19 +24,19 @@ class Finnomena(Source):
         session = HTMLSession()
         r = session.get(url)
         r.html.render()
-        price = r.html.find('h3', first=True)
+        price = r.html.find("h3", first=True)
         return float(price.text)
-    
+
     def list(self):
         # TODO
         pass
 
-class Sec(Source):
 
+class Sec(Source):
     def __init__(self, subscription_key):
         # TODO: WIP
         super().__init__()
-    
+
     def get(fund: str):
         # TODO: WIP
         pass
@@ -44,5 +44,3 @@ class Sec(Source):
     def list(self):
         # TODO: WIP
         pass
-
-        
