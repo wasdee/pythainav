ad4e2b8772309fa260bf3df8f1d5f1fd94ac7f0c
Nutchanon Ninyawee <nutchanon@codustry.com>
2021-10-03 17:34:44 +0000

    fix Literal Error

--- pythainav/sources.py
+++ pythainav/sources.py
@@ -1,4 +1,10 @@
-from typing import List, Literal
+from typing import List
+
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal
+
 
 import datetime
 from abc import ABC, abstractmethod
